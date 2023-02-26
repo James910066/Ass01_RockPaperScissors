424138cd217c664761cf1b1eeb0715c0d2260d51
James910066 <95539722+James910066@users.noreply.github.com>
2023-02-26 18:58:33 -0500

    Update RockPaperScissors.java

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -1,87 +1,102 @@
-import java.util.Scanner; //Import of Scanner
+import java.util.Scanner; //Import of Scanner Utility
 public class RockPaperScissors
 {
     public static void main(String[] args)
-    {
+    {   //Declaration and assignment of all variables
+        String playerA = "";
+        String playerB = "";
+        boolean done = false;
+        String YesOrNo = "";
+        Scanner in = new Scanner(System.in);
+        String trash = "";
+        boolean correctInputPlayerA = false;
+        boolean correctInputPlayerB = false;
 
-            String playerA = "";
-            String playerB = "";
-            boolean done = false;
-            String YesOrNo = "";
-            Scanner in = new Scanner(System.in);
-            String trash = "";
-
-        do
+        do //Do While loop asking to play again or not
         {
+            do //Start of Do While loop for testing playerA input
+            {
+                System.out.println("Player A choose one: R, P or S "); //Prompt user for input
+                if (in.hasNextInt()) //Check if user input is an integer and not string
+                    {
+                        trash = in.nextLine(); //Assignment of bad input from user
+                        System.out.println(trash + " is invalid input."); //Output statement of bad input to user
+                    }
+                else //Other choice on good input
+                    {
+                        playerA = in.nextLine(); //Assignment of correct user input
+                        correctInputPlayerA = true; //Reassign variable condition to end loop
+                    }
+            } while (!correctInputPlayerA); //Boolean check for continuing user prompt
 
-            System.out.print("Player A choose one: R, P or S ");
-            playerA = in.nextLine();
-            System.out.print("Player B choose one: R, P or S ");
-            playerB = in.nextLine();
+            do //Start of Do While loop for testing playerB input
+            {
+                System.out.println("Player B choose one: R, P or S "); //Prompt user for input
+                if (in.hasNextInt()) //Check if user input is an integer and not string
+                    {
+                        trash = in.nextLine(); //Assignment of bad input from user
+                        System.out.println(trash + " is invalid input."); //Output statement of bad input to user
+                    }
+                else //Other choice on good input
+                    {
+                        playerB = in.nextLine(); //Assignment of correct user input
+                        correctInputPlayerB = true; //Reassign variable condition to end loop
+                    }
+            } while (!correctInputPlayerB); //Boolean check for continuing user prompt
+            //Actual start of the game
+            if (playerA.equalsIgnoreCase("R")) //Check playerA choice for Rock and convert to either case
+            {
+                if (playerB.equalsIgnoreCase("R")) //Check for playerB choice for Rock and convert to either case
+                    {
+                        System.out.println("It's a tie R x R"); //Tie output statement
+                    }
+                else if (playerB.equalsIgnoreCase("P")) //Check playerB for Paper choice and convert to either case
+                    {
+                        System.out.println("Paper covers rock Player B wins"); //Winner output statement
+                    }
+                else //Last of the 3 choices that playerB could be: Scissors
+                    {
+                        System.out.println("Rock breaks scissors Player A wins"); //Winner output statement
+                    }
+            }
+            else if (playerA.equalsIgnoreCase("P")) //Check playerA choice for Paper and convert to either case
+            {
+                if (playerB.equalsIgnoreCase("R")) //Check for playerB choice for Rock and convert to either case
+                    {
+                        System.out.println("Paper covers rock Player A wins"); //Winner output statement
+                    }
+                else if (playerB.equalsIgnoreCase("P")) //Check for playerB choice for Paper and convert to either case
+                    {
+                        System.out.println("It's a tie P x P"); //Tie output statement
+                    }
+                else //Last of the 3 choices that playerB could be: Scissors
+                    {
+                        System.out.println("Scissors cuts paper Player B wins"); //Winner output statement
+                    }
+            }
+            else //Last of the 3 choices that playerA could be: Scissors
+            {
+                if (playerB.equalsIgnoreCase("R")) //Check for playerB choice for Rock and convert to either case
+                    {
+                        System.out.println("Rock breaks scissors Player B wins"); //Winner output statement
+                    }
+                else if (playerB.equalsIgnoreCase("P")) //Check for playerB choice for Paper and convert to either case
+                    {
+                        System.out.println("Scissors cuts paper Player A wins"); //Winner output statement
+                    }
+                else //Last of the 3 choices that playerB could be: Scissors
+                    {
+                        System.out.println("It's a tie S x S"); //Tie output statement
+                    }
+            }
 
-            if (playerA.equalsIgnoreCase("R"))
-            {
-                if (playerB.equalsIgnoreCase("R"))
-                    {
-                        System.out.println("It's a tie R x R");
-                    }
-                else if (playerB.equalsIgnoreCase("P"))
-                    {
-                        System.out.println("Paper covers rock Player B wins");
-                    }
-                else //playB is scissors
-                    {
-                        System.out.println("Rock breaks scissors Player A wins");
-                    }
-                System.out.print("Are you done playing? ");
-                YesOrNo = in.nextLine();
-                if(YesOrNo.equalsIgnoreCase("Y"))
+            System.out.print("Are you done playing? "); //Prompt users to play game again or not
+            YesOrNo = in.nextLine(); //User input assigned to boolean variable
+            if(YesOrNo.equalsIgnoreCase("Y")) //Convert user input to either case
                 {
-                    done = true;
+                    done = true; //Change of boolean variable from false because user chose Y or y
                 }
-            }
-            else if (playerA.equalsIgnoreCase("P"))
-            {
-                if (playerB.equalsIgnoreCase("R"))
-                    {
-                        System.out.println("Paper covers rock Player A wins");
-                    }
-                else if (playerB.equalsIgnoreCase("P"))
-                    {
-                        System.out.println("It's a tie P x P");
-                    }
-                else //playerB is scissors
-                    {
-                        System.out.println("Scissors cuts paper Player B wins");
-                    }
-                System.out.print("Are you done playing? ");
-                YesOrNo = in.nextLine();
-                if(YesOrNo.equalsIgnoreCase("Y"))
-                {
-                    done = true;
-                }
-            }
-            else //playerA is scissors
-            {
-                if (playerB.equalsIgnoreCase("R"))
-                    {
-                        System.out.println("Rock breaks scissors Player B wins");
-                    }
-                else if (playerB.equalsIgnoreCase("P"))
-                    {
-                        System.out.println("Scissors cuts paper Player A wins");
-                    }
-                else //playerB is scissors
-                    {
-                        System.out.println("It's a tie S x S");
-                    }
-                System.out.print("Are you done playing? ");
-                YesOrNo = in.nextLine();
-                if(YesOrNo.equalsIgnoreCase("Y"))
-                {
-                    done = true;
-                }
-            }
-        } while(!done);
+
+        } while (!done) ; //Boolean check to see if user's are still playing
     }
 }
